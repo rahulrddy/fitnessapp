0b5251e3341bd04bde59f6688087c5acef65bb43
Rahul Reddy Origanti <rahul.origanti@gmail.com>
2020-04-05 15:16:00 -0700

    Added Project folder structure and Added firebase to the app

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:fitnessapp/screens/autenticate/autenticate.dart';
+import 'package:fitnessapp/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
